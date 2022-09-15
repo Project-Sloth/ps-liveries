48ac7fb71caa2b2d540c582ee3b89f28f82e08c0
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-09-14 22:02:38 -0400

    First Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+
+games { 'rdr3', 'gta5' }
+
+files {
+    'data/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/*.meta'
+data_file 'CARCOLS_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/*.meta'
+data_file 'VEHICLE_SHOP_DLC_FILE' 'data/**/*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*.meta' 
+
