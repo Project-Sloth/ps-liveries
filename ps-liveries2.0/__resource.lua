451ff5548d11fdcc1e5136a97d4cc77bf7a66a80
Bunny5578 <103382613+Bunny5578@users.noreply.github.com>
2023-03-31 21:09:15 -0400

    Added ps-liveries2.0 files

---
+++ ps-liveries2.0/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+
+files {
+	'data/vehiclelayouts.meta',
+	'data/carcols.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+}
