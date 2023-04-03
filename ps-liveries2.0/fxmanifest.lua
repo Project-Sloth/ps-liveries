be251ed10e19a059f0f5d1ba800bab8a97f6a344
Bunny5578 <103382613+Bunny5578@users.noreply.github.com>
2023-04-03 16:15:07 -0400

    Ps-liveries 2.0 update (#29)
    * Added ps-liveries2.0 files
    
    * Added half of the callsign system files
    
    * Added as350 with open/closing doors
    
    ---------
    
    Co-authored-by: MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>

---
+++ ps-liveries2.0/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+
+games { 'rdr3', 'gta5' }
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+
+files {
+	'data/vehiclelayouts.meta',
+	'data/carcols.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+}
